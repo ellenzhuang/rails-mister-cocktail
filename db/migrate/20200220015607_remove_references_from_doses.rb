ad07093bb0837260d5441e9e62b44076f3d1c422
Ellen Zhuang <ellenzhuang88@gmail.com>
2020-02-20 14:07:52 +1100

    Fixed models and seeded the ingredients

--- db/migrate/20200220015607_remove_references_from_doses.rb
+++
@@ -1,6 +0,0 @@
-class RemoveReferencesFromDoses < ActiveRecord::Migration[5.2]
-  def change
-    remove_reference(:doses, :cocktail, foreign_key: true)
-    remove_reference(:doses, :ingredient, foreign_key: true)
-  end
-end
